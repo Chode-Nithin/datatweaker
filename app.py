edb8b914d0852833c7cdd6bd1b08cfe6ad3d20ae
Chode-Nithin <135514529+Chode-Nithin@users.noreply.github.com>
2024-05-01 12:12:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,8 +157,9 @@ def main():
 
         if 'Box' in plot_types:
             st.subheader("Box Plot")
-            selected_column = st.selectbox("Select a column:", df.columns)
             target_column = st.selectbox("Select a column:", df.columns)
+            selected_column = st.selectbox("Select a column:", df.columns)
+            
             # Set the height of the Box Plot
             box_height = st.slider("Select the height of the Box Plot:", min_value=1, max_value=20, value=10)
             fig_box, ax_box = plt.subplots(figsize=(10, box_height))
